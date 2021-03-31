7a525676bd6ccb920cb8d06c05730283f7708c28
Franklin Anyaso <franklin@spleet.ng>
2021-03-31 15:30:56 +0100

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+require('./bootstrap');
+
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app')
---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/js/assets/scss/app.scss', 'public/css')
+    .options({ 
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')]
+     })
+     .purgeCss({
+         enabled: mix.inProduction(),
+         folders: ['src', 'resources/js/'],
+         extensions: ['html', 'js', 'php', 'vue']
+        });
